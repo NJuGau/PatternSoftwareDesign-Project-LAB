e5ddfed184769d46e836b41db78fc56165faa6a0
JuanGau <nathanael.gauthama@binus.ac.id>
2023-02-17 22:54:27 +0700

    Last change 17/02/23: add alphanumeric validation register

--- WebService_LAB/controller/CustomerController.cs
+++ WebService_LAB/controller/CustomerController.cs
@@ -10,6 +10,33 @@ namespace WebService_LAB.controller
 {
     public class CustomerController
     {
+        public static Boolean checkAlphaNumeric(String str)
+        {
+            Boolean isAlpha = false;
+            Boolean isNumeric = false;
+            foreach (char character in str)
+            {
+                if((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z'))
+                {
+                    isAlpha = true;
+                }
+                else if (character >= '0' && character <= '9')
+                {
+                    isNumeric = true;
+                }
+                else
+                {
+                    return false;
+                }
+
+                if(isNumeric == true && isAlpha == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public static String login(String email, String password)
         {
             if (email.Length == 0)
@@ -25,7 +52,14 @@ namespace WebService_LAB.controller
 
         public static String register(String name, String email, String gender, String address, String password)
         {
-            if(name <= 5 || name >=50)
+            if(name.Length <= 5 || name.Length >= 50)
+            {
+                return JsonConvert.SerializeObject("Name must be between 5 and 50 characters!");
+            }
+            else if (!checkAlphaNumeric(password))
+            {
+                return JsonConvert.SerializeObject("Password must be alphanumeric!");
+            }
             return "ello";
         }
     }
