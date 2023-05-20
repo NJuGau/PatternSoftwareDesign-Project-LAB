4ffeffc75bc198e2503c2dfa50fa0da4d19a6277
Andrew Oroh <andreworoh.ao@gmail.com>
2023-05-20 21:41:12 +0700

    update cart

--- WebApp_LAB/factory/CartFactory.cs
+++ WebApp_LAB/factory/CartFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using WebApp_LAB.ado_model;
-namespace WebService_LAB.factory
+namespace WebApp_LAB.factory
 {
     public class CartFactory
     {
